f605f8d463145928863f67961a457a8b3c5053a8
Marcin Czenko <mczenko@me.com>
2018-03-26 18:53:04 +0200

    Squashed 'workspaces/homepage/' content from commit 614289e
    git-subtree-dir: workspaces/homepage
    git-subtree-split: 614289e505f6c40f2b00a887ef0f0d0f8da37ac3

---
+++ src/layouts/LayoutGrid.js
@@ -0,0 +1,19 @@
+import glamorous from 'glamorous'
+import { rhythm } from '../utils/typography'
+import { Grid } from '../utils/grid'
+
+let grid = new Grid([
+  'sidebar content'
+], {
+  gridTemplateColumns: '300px 800px'
+})
+
+const LayoutGrid = glamorous.div(grid.container, {
+  boxSizing: 'border-box',
+  margin: rhythm(1)
+})
+
+const SidebarGridItem = glamorous.div(grid.sidebar, { padding: '1rem' })
+const ContentGridItem = glamorous.div(grid.content, { width: '100%' })
+
+export { LayoutGrid, SidebarGridItem, ContentGridItem }
