642f4db67d6043ff24248c980b66a88c6c24d081
Pelagia Sykoudi-Amanatidou <pelagia.sykoudi@philips.com>
2018-11-05 20:10:26 +0100

    add emotion and use it on Centered component

--- workspaces/react-layout-helpers/source/Centered.js
+++ workspaces/react-layout-helpers/source/Centered.js
@@ -1,6 +1,6 @@
-import glamorous from 'glamorous'
+import styled from 'react-emotion'
 
-const Centered = glamorous.div({
+const Centered = styled('div')({
   display: 'flex',
   flexFlow: 'column nowrap',
   alignItems: 'center',
