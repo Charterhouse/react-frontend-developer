3d1a5ee28a48b8e338cb3a10fc533414f8a7c2a8
Marcin Czenko <marcin@everydayproductive.com>
2018-11-22 01:05:36 +0100

    Adds source maps when generating babel

--- tools/build.js
+++ tools/build.js
@@ -20,26 +20,26 @@ class Builder {
     console.log(`Building ${this.iifeFileName}`)
     console.log('Building CommonJS modules ...')
 
-    this.exec('babel source -d lib --delete-dir-on-start --no-babelrc', {
+    this.exec('babel source -d lib --delete-dir-on-start --no-babelrc --source-maps', {
       BABEL_ENV: 'commonjs'
     })
 
     console.log('\nBuilding ES modules ...')
 
-    this.exec('babel source -d es --delete-dir-on-start --no-babelrc', {
+    this.exec('babel source -d es --delete-dir-on-start --no-babelrc --source-maps', {
       BABEL_ENV: 'es'
     })
 
     console.log('\nBuilding IIFE module ...')
 
-    this.exec(`rollup -c -f iife -o iife/${this.iifeFileName}.js`, {
+    this.exec(`rollup -c -m -f iife -o iife/${this.iifeFileName}.js`, {
       BABEL_ENV: 'iife',
       NODE_ENV: 'development'
     })
 
     console.log('\nBuilding minimized IIFE module ...')
 
-    this.exec(`rollup -c -f iife -o iife/${this.iifeFileName}.min.js`, {
+    this.exec(`rollup -c -m -f iife -o iife/${this.iifeFileName}.min.js`, {
       BABEL_ENV: 'iife',
       NODE_ENV: 'production'
     })
